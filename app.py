0df1b590cbd26d25f368651970cd8a23ca04f76a
debjyoti ghosh <debjyoti1ghosh@gmail.com>
2025-03-19 09:59:10 +0530

    commit

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import wave
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Flask API is running on Vercel!"
+
 @app.route('/upload_audio', methods=['POST'])
 def upload_audio():
     audio_data = request.data
 
-    with wave.open("recorded_audio.wav", "wb") as wf:
+    with wave.open("/tmp/recorded_audio.wav", "wb") as wf:
         wf.setnchannels(1)  # Mono
         wf.setsampwidth(2)  # 16-bit
         wf.setframerate(16000)
@@ -15,5 +19,6 @@ def upload_audio():
 
     return "Audio received", 200
 
-if __name__ == '__main__':
-    app.run()
+# Vercel requires this to work
+def handler(event, context):
+    return app(event, context)
